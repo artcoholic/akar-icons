e260d6bac9ced992649740195fa6d6f67a847721
Arturo Wibawa <agwibawa@gmail.com>
2020-10-31 00:56:19 -0700

    fixed mobile view

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,21 +5,34 @@ import Search from './Search';
 
 const Header = styled.header`
   display: block;
-  padding: 12px 24px;
+  padding: 12px 16px;
   border-bottom: 1px solid #f2f2f2;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  @media (min-width: 768px) {
+    padding: 12px 24px;
+  }
 `
 const Title = styled.h1`
   margin: 0;
+  font-size: 1em;
+  white-space: nowrap;
+  @media (min-width: 768px) {
+   font-size: 2em;
+  }
 `
 
 const Version = styled.span`
   border: 1px solid #e2e2e2;
-  border-radius: 4px;
-  padding: 4px 8px;
+  border-radius: 2px;
+  padding: .35em .5em;
   font-weight:bolder;
+  font-size: .5em;
+  @media (min-width: 768px) {
+   font-size: 1em;
+   border-radius: 4px;
+  }
 `
 
 export default ({ query, updateQuery, icons }) =>
--- src/components/Search.js
+++ src/components/Search.js
@@ -7,24 +7,40 @@ const Container = styled.div`
   align-items: center;
   flex-grow: 2;
   color: rgba(0,0,0,0.5);
-  margin-left:24px;
-  padding: 2px 24px;
+  margin-left:12px;
+  padding: 0px 12px;
   border-left: 2px solid #e2e2e2;
+  svg {
+    width: 14px;
+    height: 14px;
+  }
+  @media (min-width: 768px) {
+    margin-left:24px;
+    padding: 2px 24px;
+    svg {
+      width: 18px;
+      height: 18px;
+    }
+  }
 `
 
 const SearchInput = styled.input`
   border: none; 
   border-radius: 4px;
-  margin-left: 8px;
+  margin-left: 4px;
   padding: 0;
   width: 100%;
-  font-size: 18px;
+  font-size: 14px;
   font-weight: 300;
   &:focus {
     outline: none;
   }
   &::placeholder  {
     color:rgba(0,0,0,0.5);
+  }
+  @media (min-width: 768px) {
+    font-size: 18px;
+    margin-left: 8px;
   }
 
 `
@@ -35,7 +51,7 @@ export default ({ query, updateQuery, icons }) => {
   }
   return (
     <Container>
-      <icons.Search size={18} />
+      <icons.Search />
       <SearchInput type="text" autocomplete="off" value={query} onChange={onSearch} placeholder="Search" />
     </Container>
   )
