91ce162f0f210249b86284a9c14d3afe43178ef7
Arturo Wibawa <agwibawa@gmail.com>
2021-07-02 21:41:16 -0700

    add aria-label

--- src/components/Header.js
+++ src/components/Header.js
@@ -178,7 +178,7 @@ const Header = ({
           <Title>Perfectly rounded icon library made for designers and developers</Title>
           <SubTitle>Use these icons to bring life into your user interface and experience. Free to use for your personal and commercial projects. Start creating!</SubTitle>
         </Wrapper>
-        <Toggle onClick={themeToggler}>
+        <Toggle onClick={themeToggler} aria-label="Theme Toggle">
           {theme === 'light' ? <icons.MoonFill /> : <icons.SunFill />}
         </Toggle>
         <Wrapper
